b49fcfbc7ade054d7de8b6b799a6465af7d6c874
Abhishek Prasad <abhishekprasad1606@gmail.com>
2024-04-05 22:01:39 +0530

    6th Change

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,14 +30,18 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
 
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if  bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            print(len(self.bullets))
+    def _update_bullets(self):
+        """Update position of the bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if  bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -56,10 +60,11 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
+
 
     def _check_keyup_events(self, event):
         """Respond to key release."""
@@ -70,8 +75,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Creates a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen. """
